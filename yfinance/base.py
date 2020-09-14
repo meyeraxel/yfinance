fe8c9abdb685226aa3833b23bbfa4637125ec545
meyeraxel <meyeraxel@johndeere.com>
2020-09-14 09:42:37 +0200

    Added arguments to prevent unknown error
    unknown error: DevToolsActivePort file doesn't exist

--- yfinance/base.py
+++ yfinance/base.py
@@ -41,6 +41,10 @@ from seleniumrequests import Chrome
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920x1080")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-infobars")
 chrome_driver = '/usr/bin/chromedriver'
 driver = Chrome(options=chrome_options, executable_path=chrome_driver)
 
